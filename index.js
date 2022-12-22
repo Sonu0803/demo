b6f749a176032c95b2d44441591812f9b45264a7
Sonu0803 <102642963+Sonu0803@users.noreply.github.com>
2022-12-22 16:23:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hello: "Agrisathi From Prod Server." }); 
+});
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, function() {
+  console.log(`App listening on port ${PORT}`); 
+});
+ 
+
